086c59738aed77624f90f1bc20cb02f8a5fea9b1
Xeno <xenocidechan@gmail.com>
2020-03-12 15:41:51 +0100

    Finished the statements

--- choice.js
+++ choice.js
@@ -1,12 +1,36 @@
-var agree = 0;
-var disagree = 0;
-var none = 0;
-var skip = 0;
-
 var counter = 0;
 var choices = [];
 
+var pro = "pro";       // agree
+var contra = "contra"; // disagree
+var neither = "none";
+var skip = "skipped";
+
+load_statement(counter);
 function load_statement(choice){
-    title.subjects[counter].title;
-    statement.subjects[counter].statement;
+    stellingtitle.innerText = subjects[counter].title;
+    stellingstatement.innerText = subjects[counter].statement;
+}
+
+function choose(choice){
+    if(counter !== 29){
+        choices[counter] = choice
+        counter++
+        load_statement(counter)
+        
+
+        console.log(choices)
+        console.log(counter)
+    }else {
+        console.log('I need to change the page now.');
+    }
+}
+
+function back(){
+    if(counter !== 0){
+        counter--
+        load_statement(counter)
+    }else {
+        backbutton.href= "homepage.html"
+    }
 }
