9fceca7e5909ddf0eb6a9687f094402b86802903
Xeno <Xenocidechan@gmail.com>
2020-03-12 10:56:23 +0100

    Did a tad bit js and changed some filenames

---
+++ choice.js
@@ -0,0 +1,12 @@
+var agree = 0;
+var disagree = 0;
+var none = 0;
+var skip = 0;
+
+var counter = 0;
+var choices = [];
+
+function load_statement(choice){
+    title.subjects[counter].title;
+    statement.subjects[counter].statement;
+}
