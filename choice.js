c06e5133369c534a3e7bcbf1561af67f2db44a11
Xeno <xenocidechan@gmail.com>
2020-03-17 11:53:36 +0100

    did some counting

--- choice.js
+++ choice.js
@@ -34,3 +34,35 @@ function back(){
         backbutton.href= "homepage.html"
     }
 }
+
+// tryna remember
+
+// from data_array.js I need:
+// title - statement - parties - amount of pro/contra/none from every party
+
+// count my total pro/contra/none
+// count party total pro/contra/none
+
+// PVV x14 pro
+// SP x13 pro
+// D66 x11 pro
+// Groenlinks x15 pro
+// Partij voor de Dieren x16 pro
+// 50Plus x15 pro
+// VNL x15 pro
+// Nieuwe Wegen x14 pro
+// Forum voor Democratie x18 pro
+// De Burger Beweging x16 pro
+// Vrijzinnige Partij x12 pro
+// Piratenpartij x9 pro
+// Libertarische Partij x14 pro
+// Lokaal in de Kamer x16 pro
+// Niet Stemmers x30 none
+// VVD x11 pro x19 contra
+// PvdA x
+// CDA
+// ChristenUnie
+// SGP
+// OndernemersPartij
+// DENK
+// Artikel 1
